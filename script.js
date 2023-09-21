bd4a3f1b7da4f2f508f0c6945b32450a5dbc640a
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-21 13:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const table = document.getElementById("table");
+const btn = document.getElementById("add")
+const name = document.getElementById("item-name-input")
+const price = document.getElementById("item-price-input")
+const total= document.getElementById("total");
+let sum = 0;
+
+btn.addEventListener("click",() =>{
+	tr = document.createElement("tr");
+	td1 = document.createElement("td")
+	td2 = document.createElement("td")
+	td1.innerText = name.value;
+	name.value = "";
+	td2.innerText = price.value;
+	sum += Number(price.value);
+    total.innerText = sum;
+	price.value = "";
+
+	tr.append(td1,td2);
+	table.append(tr);
+	
+
+	
+})
